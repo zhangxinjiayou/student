1001db44702dbe4a8e8a9c0df804b4216ba4cb79
zhangxin <zhangxin@etoak.com>
2020-10-05 18:49:35 +0800

    在家开发签到功能的其他功能50%

---
+++ SignIn.java
@@ -0,0 +1,16 @@
+/*
+* @Author: Marte
+* @Date:   2020-10-05 18:19:04
+* @Last Modified by:   Marte
+* @Last Modified time: 2020-10-05 18:46:26
+*/
+
+public class SigIn {
+    private String name;
+    private String singTime;
+
+    public void sigb(){
+        System.out.println(singTime);
+    }
+
+}
